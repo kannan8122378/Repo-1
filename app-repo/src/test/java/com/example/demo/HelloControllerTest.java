071bc35998b31dc9464ea86757491463185aa926
kannan8122378 <kannan.techsys@gmail.com>
2026-02-26 08:31:19 +0000

    Add unit test for HelloController

--- app-repo/src/test/java/com/example/demo/HelloControllerTest.java
+++ app-repo/src/test/java/com/example/demo/HelloControllerTest.java
@@ -1,1 +1,13 @@
+package com.example.demo;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloControllerTest {
+
+  @Test
+  void testMessage() {
+    HelloController c = new HelloController();
+    assertTrue(c.home().contains("Jenkins"));
+  }
+}
